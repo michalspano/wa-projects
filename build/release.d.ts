7f620fa33eb248da2e0f3f972a0f06459cc4f684
michalspano <speko.spano@gmail.com>
2022-07-07 15:55:06 +0200

    Include `buidl` with `release`

---
+++ build/release.d.ts
@@ -0,0 +1,11 @@
+/**
+ * assembly/index/hello
+ * @returns `~lib/string/String`
+ */
+export declare function hello(): string;
+/**
+ * assembly/index/factorial
+ * @param n `i32`
+ * @returns `i32`
+ */
+export declare function factorial(n: number): number;
